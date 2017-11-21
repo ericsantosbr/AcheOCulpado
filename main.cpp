dee09b89ea991a45d8356231cffc5f2d74099ad5
Eric Santos <ericsantosbr2@gmail.com>
2017-11-21 01:12:50 -0200

    Criados links para as imagens, todo atualizado

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 /*
-TODO: INSERIR LINKS PARA AS IMAGENS
+TODO: CONSERTAR AS ESCALAS DAS IMAGENS
+
+DONE: INSERIR LINKS PARA AS IMAGENS
 	  
 */
 
@@ -19,6 +21,7 @@ void *creditos, *creditos_m, *iniciar, *iniciar_m, *opcoes, *opcoes_m, *sair, *s
 // Funcoes
 void configura();
 void imagens();
+int escala(int x, int y, int maxy);
 void falas_pt();
 void falas_en();
 void configuraimgs();
@@ -43,26 +46,65 @@ void configura(){
 }
 
 void imagens(){
-	setactivepage(3);
-	int tam = imagesize(0, 0, 211, 59);
-	// TESTE: REMOVER O SETVISUALPAGE
-	setvisualpage(3);
-	setfillstyle(2, WHITE);
-	bar(0, 0, getmaxx(), getmaxy());
+	// Imagens da tela inicial terão um padrão de altura 60, com largura definida pela função escala()
+	int scale = escala(619, 175, 40);
+	int tam = imagesize(0, 0, scale, 60);
 	iniciar = malloc(tam);
+	readimagefile(".//cortes//iniciar.gif", 0, 0, scale, 60);
+	
+	getimage(0, 0, scale - 1, 59, iniciar);
 	iniciar_m = malloc(tam);
-	readimagefile(".//cortes//iniciar.gif", 0, 0, 212, 60);
-	getimage(0, 0, 211, 59, iniciar);
+	readimagefile(".//cortes//iniciar_m.gif", 0, 0, scale, 60);
+	getimage(0, 0, scale - 1, 59, iniciar_m);
+	
+	
+	scale = escala(787, 203, 40);
+	tam = imagesize(0, 0, scale, 60);
+	creditos = malloc(tam);
+	readimagefile(".//cortes//creditos.gif", 0, 0, scale, 60);
+	getimage(0, 0, scale - 1, 59, creditos);
+
+	creditos_m = malloc(tam);
+	readimagefile(".//cortes//creditos_m.gif", 0, 0, scale, 60);
+	getimage(0, 0, scale - 1, 59, creditos_m);
+
+
+	scale = escala(629, 239, 40);
+	tam = imagesize(0, 0, scale, 60);
+	opcoes = malloc(tam);
+	readimagefile(".//cortes//opcoes.gif", 0, 0, scale, 60);
+	getimage(0, 0, scale - 1, 59, opcoes);
+
+	opcoes_m = malloc(tam);
+	readimagefile(".//cortes//opcoes_m.gif", 0, 0, scale, 60);
+	getimage(0, 0, scale - 1, 59, opcoes_m);
+	
+
+	scale = escala(384, 168, 40);
+	tam = imagesize(0, 0, scale, 60);
+	sair = malloc(tam);
+	readimagefile(".//cortes//sair.gif", 0, 0, scale, 60);
+	getimage(0, 0, scale - 1, 59, sair);
+
 	getch();
-	readimagefile(".//cortes//iniciar_m.gif", 0, 0, 212, 60);
-	getimage(0, 0, 211, 59, iniciar_m);
-	getch();
-	setactivepage(0);
+	
+	sair_m = malloc(tam);
+	readimagefile(".//cortes//sair_m.gif", 0, 0, scale, 60);
+	getimage(0, 0, scale - 1, 59, sair_m);
+}
+
+// Calcula as escalas das imagens da tela de inicio
+int escala(int x, int y, int maxy){
+	int saida;
+	saida = (x * maxy) / y;
+	printf("\n%d\n", saida);
+	return saida;
 }
 
 // Tela inicial do jogo
 void inicio(){
-	int i, j, x, y, keep;
+	int i, j, x, y, keep, passoy;
+	passoy = 90;
 	keep = 1;
 	x = 10;
 	y = 10;
@@ -71,12 +113,28 @@ void inicio(){
 		else setactivepage(0);
 		cleardevice();
 		if(GetKeyState(VK_ESCAPE) & 0x80) keep = 0;
+				
+		// Fundo
 		
-		putimage(60, 60, iniciar_m, AND_PUT);
-		putimage(60, 60, iniciar, OR_PUT);
+		// Plot das Opcoes
+		x = 150;
+		y = 60;
 		
-		x = 10;
-		y = 10;
+		putimage(x, y, iniciar_m, AND_PUT);
+		putimage(x, y, iniciar, OR_PUT);
+		
+		y += passoy;
+		putimage(x, y, opcoes_m, AND_PUT);
+		putimage(x, y, opcoes, OR_PUT);
+		
+		y += passoy;
+		putimage(x, y, creditos_m, AND_PUT);
+		putimage(x, y, creditos, OR_PUT);
+		
+		y += passoy;
+		putimage(x, y, sair_m, AND_PUT);
+		putimage(x, y, sair, OR_PUT);
+		
 		if(getactivepage() == 0) setvisualpage(0);
 		else setvisualpage(1);
 	}
